ac48ebbc39f5669f35ab459e5891736e6b774933
corsair <corsair954@mail.ru>
2026-02-01 21:17:09 +0300

    7. Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -2,8 +2,8 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.repository.CandidateRepository;
 import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
 
@@ -23,4 +23,42 @@ public class CandidateController {
     public String getCreationPage() {
         return "candidates/create";
     }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Candidate candidate) {
+        repository.save(candidate);
+        return "redirect:/candidates";
+    }
+
+    @PostMapping("/update")
+    public String updateCandidate(@ModelAttribute Candidate candidate, Model model) {
+        var isUpdated = repository.update(candidate);
+        if (!isUpdated) {
+            model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable int id, Model model) {
+        var optionalCandidate = repository.findById(id);
+        if (optionalCandidate.isEmpty()) {
+            model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        model.addAttribute("candidate", optionalCandidate.get());
+        return "candidates/one";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteById(@PathVariable int id, Model model) {
+        var isDeleted = repository.deleteById(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
+    }
+
 }
