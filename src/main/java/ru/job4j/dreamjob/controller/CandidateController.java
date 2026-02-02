c40e529c819498fd931620921fc3c756f84c4823
corsair <corsair954@mail.ru>
2026-02-02 13:44:03 +0300

    1. Слоеная архитектура. [#504851]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -6,16 +6,18 @@ import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.repository.CandidateRepository;
 import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
+import ru.job4j.dreamjob.service.CandidateService;
+import ru.job4j.dreamjob.service.SimpleCandidateService;
 
 @Controller
 @RequestMapping("/candidates")
 public class CandidateController {
 
-    private final CandidateRepository repository = MemoryCandidateRepository.getInstance();
+    private final CandidateService candidateService = SimpleCandidateService.getInstance();
 
     @GetMapping
     public String getAll(Model model) {
-        model.addAttribute("candidates", repository.findAll());
+        model.addAttribute("candidates", candidateService.findAll());
         return "candidates/list";
     }
 
@@ -26,13 +28,13 @@ public class CandidateController {
 
     @PostMapping("/create")
     public String create(@ModelAttribute Candidate candidate) {
-        repository.save(candidate);
+        candidateService.save(candidate);
         return "redirect:/candidates";
     }
 
     @PostMapping("/update")
     public String updateCandidate(@ModelAttribute Candidate candidate, Model model) {
-        var isUpdated = repository.update(candidate);
+        var isUpdated = candidateService.update(candidate);
         if (!isUpdated) {
             model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
             return "errors/404";
@@ -42,7 +44,7 @@ public class CandidateController {
 
     @GetMapping("/{id}")
     public String findById(@PathVariable int id, Model model) {
-        var optionalCandidate = repository.findById(id);
+        var optionalCandidate = candidateService.findById(id);
         if (optionalCandidate.isEmpty()) {
             model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
             return "errors/404";
@@ -53,7 +55,7 @@ public class CandidateController {
 
     @GetMapping("/delete/{id}")
     public String deleteById(@PathVariable int id, Model model) {
-        var isDeleted = repository.deleteById(id);
+        var isDeleted = candidateService.deleteById(id);
         if (!isDeleted) {
             model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
             return "errors/404";
