61ffc85aadd0960055a37426a5d7264cf4adf907
corsair <corsair954@mail.ru>
2026-02-05 16:08:26 +0300

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859 #557843]

--- src/main/java/ru/job4j/dreamjob/repository/Sql2oVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oVacancyRepository.java
@@ -41,8 +41,7 @@ public class Sql2oVacancyRepository implements VacancyRepository {
         try (var connection = sql2o.open()) {
             var query = connection.createQuery("DELETE FROM vacancies WHERE id = :id");
             query.addParameter("id", id);
-            query.executeUpdate();
-            return true;
+            return query.executeUpdate().getResult() != 0;
         }
     }
 
